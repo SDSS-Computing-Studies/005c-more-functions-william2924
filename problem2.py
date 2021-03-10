aaaa74ae560e8ea230a262b25d6855d8dbebbe9a
william2924 <78515221+william2924@users.noreply.github.com>
2021-03-10 13:55:54 -0800

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -15,8 +15,8 @@ assert hypotenuse(5,3,True) == 4
 """
 import math
 
-def hypotenuse(x,y,sign):
+def hypotenuse(x, y, sign):
     if sign:
-        return math.pow(math.pow(max(x,y),2) - math.pow(min(x,y),1/2),1/2)
+        return math.pow(math.pow(max(x, y),2) - math.pow(min(x,y), 2), 1/2)
     else:
         return math.pow((math.pow(x,2) + math.pow(y,2)),1/2)
