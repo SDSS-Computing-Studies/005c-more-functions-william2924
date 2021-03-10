de77f6adb6248cf8fe59d2662d7f9648dc6576eb
william2924 <78515221+william2924@users.noreply.github.com>
2021-03-10 13:31:40 -0800

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -17,6 +17,6 @@ import math
 
 def hypotenuse(x,y,sign):
     if sign:
-        return math.pow(math.pow(max(x,y),2) - math.pow(min(x,y),1/2),
+        return math.pow(math.pow(max(x,y),2) - math.pow(min(x,y),1/2),1/2)
     else:
         return math.pow((math.pow(x,2) + math.pow(y,2)),1/2)
